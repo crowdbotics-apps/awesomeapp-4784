d0ce294e2a56b608473b936a0645e730d4035260
Crowdbotics <team@crowdbotics.com>
2019-06-17 14:06:07 +0000

    initial commit

---
+++ android/app/src/main/java/com/awesomeapp_4784/MainActivity.java
@@ -0,0 +1,15 @@
+package com.awesomeapp_4784;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "awesomeapp_4784";
+    }
+}
---
+++ backend/awesomeapp_4784/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for awesomeapp_4784 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'awesomeapp_4784.settings')
+
+application = get_wsgi_application()
